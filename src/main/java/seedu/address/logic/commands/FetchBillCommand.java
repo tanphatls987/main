9cd26c4a71e338cf158384a9e62259ab622f740d
hmtey <huimingtey@u.nus.edu>
2020-03-17 19:30:31 +0800

    Updated skeleton for FetchBillCommand

--- src/main/java/seedu/address/logic/commands/FetchBillCommand.java
+++ src/main/java/seedu/address/logic/commands/FetchBillCommand.java
@@ -2,6 +2,8 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
 import seedu.address.model.hotel.person.Person;
 
 /**
@@ -11,10 +13,17 @@ public class FetchBillCommand extends Command{
 
     public static final String COMMAND_WORD = "bill";
 
+    public static final String MESSAGE_SUCCESS = "This is the bill for the guest.";
+
     /**
      *
      */
-    public FetchBillCommandCommand(Person person) {
-        requireNonNull(person);
+    public FetchBillCommand() {
+        //requireNonNull(person);
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        return new CommandResult(String.format(MESSAGE_SUCCESS));
     }
 }
