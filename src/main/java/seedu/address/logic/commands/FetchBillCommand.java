0f89b82676794e0e5d60f57562fb36906aded9e6
hmtey <huimingtey@u.nus.edu>
2020-03-17 19:42:01 +0800

    Fixed checkstyle errors

--- src/main/java/seedu/address/logic/commands/FetchBillCommand.java
+++ src/main/java/seedu/address/logic/commands/FetchBillCommand.java
@@ -1,15 +1,15 @@
 package seedu.address.logic.commands;
 
-import static java.util.Objects.requireNonNull;
+//import static java.util.Objects.requireNonNull;
 
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
-import seedu.address.model.hotel.person.Person;
+//import seedu.address.model.hotel.person.Person;
 
 /**
  * Retrieves the bill of a guest
  */
-public class FetchBillCommand extends Command{
+public class FetchBillCommand extends Command {
 
     public static final String COMMAND_WORD = "bill";
 
