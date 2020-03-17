cc34cfb36d5d5fc610a95fb3c5cadb197fa4b065
Sanchari Bandyopadhyay <sancharizxc@gmail.com>
2020-03-17 15:30:31 +0800

    removed unecessary field

--- src/test/java/seedu/address/testutil/RoomBuilder.java
+++ src/test/java/seedu/address/testutil/RoomBuilder.java
@@ -2,7 +2,6 @@ package seedu.address.testutil;
 
 import seedu.address.model.hotel.Room;
 import seedu.address.model.hotel.Tier;
-import seedu.address.model.hotel.person.Person;
 import seedu.address.model.ids.RoomId;
 
 
@@ -10,19 +9,16 @@ import seedu.address.model.ids.RoomId;
  * A utility class to help build rooms
  */
 public class RoomBuilder {
-    public static final Person ALICE = TypicalPersons.ALICE;
     public static final Tier DEFAULT_TIER = new Tier();
     public static final String DEFAULT_ROOMNUM = "1";
     public static final RoomId DEFAULT_ROOMID = RoomId.generate(DEFAULT_ROOMNUM);
 
-    private Person person;
     private Tier roomTier;
     private String roomNum;
     private RoomId roomId;
 
 
     public RoomBuilder() {
-        person = ALICE;
         roomTier = DEFAULT_TIER;
         roomNum = DEFAULT_ROOMNUM;
         roomId = DEFAULT_ROOMID;
