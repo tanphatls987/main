850eabbf01e6a9550dec1a85cbaaffd29be026ec
Daniel Alfred Widjaja <dalfred2000@gmail.com>
2020-03-20 19:05:05 +0800

    Update MatchNamePredicateTest.java

--- src/test/java/seedu/address/model/hotel/person/MatchNamePredicateTest.java
+++ src/test/java/seedu/address/model/hotel/person/MatchNamePredicateTest.java
@@ -8,8 +8,8 @@ import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
-import seedu.address.testutil.TypicalPersons.ALICE;
-import seedu.address.testutil.TypicalPersons.BENSON;
+import static seedu.address.testutil.TypicalPersons.ALICE;
+import static seedu.address.testutil.TypicalPersons.BENSON;
 
 class MatchNamePredicateTest {
 
