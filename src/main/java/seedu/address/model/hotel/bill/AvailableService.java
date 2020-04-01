29a4b04a80a3bb1950bd4a36cb6714b9617dc2a2
Tuan Le <lequangtuan6b@gmail.com>
2020-04-02 00:27:35 +0800

    addAvailableServices

---
+++ src/main/java/seedu/address/model/hotel/bill/AvailableService.java
@@ -0,0 +1,33 @@
+package seedu.address.model.hotel.bill;
+
+import seedu.address.model.ids.AvailableServiceId;
+
+/**
+ * Available services, to be added when initialization.
+ * Stores: description, cost, id of the service.
+ */
+public class AvailableService {
+    private String description;
+    private Cost cost;
+    private AvailableServiceId id;
+
+    /**
+     *
+     * @param desc: a short description of the service
+     * @param cost: amount
+     * @param id: for reference
+     */
+    public AvailableService(String desc, Cost cost, AvailableServiceId id) {
+        this.description = desc;
+        this.cost = cost;
+        this.id = id;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public Cost getCost() {
+
+    }
+}
