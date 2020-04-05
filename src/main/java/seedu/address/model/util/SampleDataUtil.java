17af7d04c62634462d1ce7300c128a9da752d4ed
tanphatls987 <phattt192011@gmail.com>
2020-04-05 16:43:30 +0800

    Fix regex expression for cost, fix setroomcost.

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -9,8 +9,6 @@ import seedu.address.model.AddressBook;
 import seedu.address.model.Hotel;
 import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.ReadOnlyHotel;
-
-import seedu.address.model.hotel.bill.Cost;
 import seedu.address.model.hotel.bill.RoomCost;
 import seedu.address.model.hotel.booking.Booking;
 import seedu.address.model.hotel.person.Email;
@@ -59,12 +57,12 @@ public class SampleDataUtil {
 
     public static Room[] getSampleRooms() {
         return new Room[] {
-            new Room("001", new Tier("GOLD"), new RoomCost(new Cost("150.00"))),
-            new Room("002", new Tier("SILVER"), new RoomCost(new Cost("100.00"))),
-            new Room("003", new Tier("SILVER"), new RoomCost(new Cost("100.00"))),
-            new Room("004", new Tier("BRONZE"), new RoomCost(new Cost("50.00"))),
-            new Room("005", new Tier("BRONZE"), new RoomCost(new Cost("50.00"))),
-            new Room("006", new Tier("BRONZE"), new RoomCost(new Cost("50.00")))
+            new Room("001", new Tier("gold"), new RoomCost("150.00")),
+            new Room("002", new Tier("silver"), new RoomCost("100.00")),
+            new Room("003", new Tier("silver"), new RoomCost("100.00")),
+            new Room("004", new Tier("bronze"), new RoomCost("50.00")),
+            new Room("005", new Tier("bronze"), new RoomCost("50.00")),
+            new Room("006", new Tier("bronze"), new RoomCost("50.00"))
         };
     }
 
--- src/test/java/seedu/address/logic/commands/SetRoomCostCommandTest.java
+++ src/test/java/seedu/address/logic/commands/SetRoomCostCommandTest.java
@@ -1,12 +1,57 @@
 package seedu.address.logic.commands;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static seedu.address.testutil.Assert.assertThrows;
+
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.hotel.bill.RoomCost;
+import seedu.address.model.hotel.room.Room;
+import seedu.address.model.hotel.room.Tier;
+import seedu.address.model.ids.RoomId;
+import seedu.address.testutil.TypicalRooms;
+
 class SetRoomCostCommandTest {
+    private Model modelStub = new ModelStubSetRoomCost();
 
     @Test
-    void execute() {
+    void executeSuccess() throws Exception {
+
+        Room bestRoom = TypicalRooms.BEST_ROOM;
+        String newPrice = "24.00";
+        SetRoomCostCommand command = new SetRoomCostCommand(bestRoom.getRoomId(), new RoomCost(newPrice));
+        String expectedMessage = String.format(EditCommand.MESSAGE_EDIT_PERSON_SUCCESS,
+            bestRoom.getRoomId(), newPrice);
+
+        CommandResult commandResult = command.execute(modelStub);
+        assertEquals(commandResult.getFeedbackToUser(),
+            String.format(SetRoomCostCommand.MESSAGE_SUCCESS,
+                bestRoom.getRoomNum(), newPrice)
+        );
+    }
+
+    @Test
+    void executeFailure_negative_price() {
+        SetRoomCostCommand command = new SetRoomCostCommand(new RoomId("nonexist"),
+            new RoomCost("-2.00")
+        );
+        assertThrows(CommandException.class,
+            SetRoomCostCommand.MESSAGE_ROOM_COST_INVALID, () -> command.execute(modelStub)
+        );
+    }
+    private class ModelStubSetRoomCost extends ModelStub {
+        @Override
+        public Optional<Room> findRoom(RoomId roomNum) {
+            return Optional.of(new Room(roomNum.toString(), new Tier(), new RoomCost()));
+        }
+
+        @Override
+        public void setRoomCost(Room room, RoomCost cost) {
+
+        }
     }
 }
