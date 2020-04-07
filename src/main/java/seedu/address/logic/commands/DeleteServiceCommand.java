9789ff32df92ed46fdd62a22ac34d79fb527552c
Tuan Le <lequangtuan6b@gmail.com>
2020-04-07 23:05:06 +0800

    add

---
+++ src/main/java/seedu/address/logic/commands/DeleteServiceCommand.java
@@ -0,0 +1,12 @@
+package seedu.address.logic.commands;
+
+/**
+ * Deletes an available services
+ */
+public class DeleteServiceCommand extends Command {
+    public static final String COMMAND_WORD = "deleteservice";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": delete an available service from hotel"
+            + "Parameters "
+            + ""
+}
