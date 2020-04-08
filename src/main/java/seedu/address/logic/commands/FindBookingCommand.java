3cd7e6f4dea4249d98c51f9fad065f3f7ef6eaae
Tuan Le <lequangtuan6b@gmail.com>
2020-04-08 22:43:32 +0800

    addFindBookingCommand

--- src/main/java/seedu/address/logic/commands/FindBookingCommand.java
+++ src/main/java/seedu/address/logic/commands/FindBookingCommand.java
@@ -1,8 +1,77 @@
 package seedu.address.logic.commands;
 
+import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.*;
+
+import java.util.HashSet;
+
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.hotel.person.Name;
+import seedu.address.model.ids.PersonId;
+import seedu.address.model.ids.RoomId;
+
 /**
  * Finds and lists all bookings in hotel whose matches specified bookers of rooms.
  */
-public class FindBookingCommand {
-    
+public class FindBookingCommand extends Command {
+
+    public static final String COMMAND_WORD = "findbooking";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": returns all bookings with matching personID"
+            + " or matching name or matching rooms.\n"
+            + "Parameters: [i/ID] ... [i/ID] [n/NAME] ... [n/NAME] [rn/ROOM_ID] ... [rn/ROOM_ID]\n"
+            + "Example: " + COMMAND_WORD + " "
+            + PREFIX_ID + "A000000 "
+            + PREFIX_ROOMNUMBER + "001";
+
+    private final HashSet<Name> names;
+    private final HashSet<PersonId> personIds;
+    private final HashSet<RoomId> roomIds;
+
+
+    /**
+     * Constructs a FindBookingCommand with list of predicates.
+     * @param names: list of names.
+     * @param personIds: list of personIds.
+     * @param roomIds: list of roomIds.
+     */
+    public FindBookingCommand(HashSet<Name> names, HashSet<PersonId> personIds, HashSet<RoomId> roomIds) {
+        requireNonNull(names);
+        requireNonNull(personIds);
+        requireNonNull(roomIds);
+
+        this.names = names;
+        this.personIds = personIds;
+        this.roomIds = roomIds;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+
+
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        if (!(other instanceof FindBookingCommand)) {
+            return false;
+        }
+
+        FindBookingCommand otherCommand = (FindBookingCommand) other;
+        return otherCommand.names.equals(this.names)
+                && otherCommand.roomIds.equals(this.roomIds)
+                && otherCommand.personIds.equals(personIds);
+    }
+
+    @Override
+    public String toString() {
+        return names.toString() + " " + roomIds.toString() + " " + personIds.toString();
+    }
+
 }
---
+++ src/main/java/seedu/address/model/hotel/person/MatchRoomPredicate.java
@@ -0,0 +1,57 @@
+package seedu.address.model.hotel.person;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.function.Predicate;
+
+import seedu.address.model.hotel.room.Room;
+import seedu.address.model.ids.RoomId;
+
+/**
+ * Check if the person name is in a predetermine list.
+ */
+public class MatchRoomPredicate implements Predicate<Room> {
+    private final HashSet<RoomId> roomIds;
+
+    /**
+     * Creates new MatchRoomPredicate to match a room in roomIds.
+     * @param roomIds: a set of ids.
+     */
+    public MatchRoomPredicate(HashSet<RoomId> roomIds) {
+        this.roomIds = roomIds;
+    }
+
+    /**
+     * Alternative constructor, given a list of roomIds instead of a set.
+     * @param roomIdList: list of roomId.
+     */
+    public MatchRoomPredicate(List<RoomId> roomIdList) {
+        this.roomIds = new HashSet<>(roomIdList);
+    }
+
+    /**
+     *
+     * @param room: a room
+     * @return {@code true} if the input argument matches the predicate,
+     * {@code false} otherwise.
+     */
+    @Override
+    public boolean test(Room room) {
+        return roomIds.contains(room.getRoomId());
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        if (!(other instanceof MatchRoomPredicate)) {
+            return false;
+        }
+
+        MatchRoomPredicate otherPredicate = (MatchRoomPredicate) other;
+        return otherPredicate.roomIds.equals(this.roomIds);
+    }
+
+}
