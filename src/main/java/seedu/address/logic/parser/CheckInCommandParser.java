d712660cb91a52b7ad256683f27321fd9c17a0f4
Daniel Alfred Widjaja <dalfred2000@gmail.com>
2020-04-08 15:46:55 +0800

    move to another branch

--- src/main/java/seedu/address/logic/parser/CheckInCommandParser.java
+++ src/main/java/seedu/address/logic/parser/CheckInCommandParser.java
@@ -27,8 +27,7 @@ public class CheckInCommandParser implements Parser<CheckInCommand> {
         ArgumentMultimap argMultimap =
             ArgumentTokenizer.tokenize(args, PREFIX_ID, PREFIX_ROOMNUMBER, PREFIX_TODATE);
 
-        if ((!arePrefixesPresent(argMultimap, PREFIX_ID, PREFIX_ROOMNUMBER, PREFIX_TODATE)
-            && (!arePrefixesPresent(argMultimap, PREFIX_BOOKING))
+        if (!arePrefixesPresent(argMultimap, PREFIX_ID, PREFIX_ROOMNUMBER, PREFIX_TODATE)
             || !argMultimap.getPreamble().isEmpty()) {
             throw new ParseException(
                 String.format(MESSAGE_INVALID_COMMAND_FORMAT,
