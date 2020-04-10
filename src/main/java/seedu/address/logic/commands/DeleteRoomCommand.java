3775ec46a33befa9b79e846424fcab47c842fd03
Tuan Le <lequangtuan6b@gmail.com>
2020-04-11 06:18:41 +0800

    addServiceResponse

--- src/main/java/seedu/address/logic/commands/DeleteRoomCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteRoomCommand.java
@@ -6,6 +6,8 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_ROOMNUMBER;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 
+
+
 /**
  * Deletes a room from hotel
  */
@@ -43,6 +45,7 @@ public class DeleteRoomCommand extends Command {
         }
 
         model.deleteRoom(toDelete);
+
         return new CommandResult(String.format(MESSAGE_SUCCESS, toDelete), "room");
     }
 
