2201dfd4c2121ee823644b249c139b5ed361b181
Tuan Le <lequangtuan6b@gmail.com>
2020-04-12 05:13:41 +0800

    updateParser

--- src/main/java/seedu/address/logic/commands/DeleteServiceCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteServiceCommand.java
@@ -1,13 +1,18 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_DESCRIPTION;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_SERVICEID;
 
+import java.util.List;
+
+import seedu.address.commons.core.either.Either;
+import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.ReadOnlyHotel;
+import seedu.address.model.hotel.bill.AvailableService;
 import seedu.address.model.ids.AvailableServiceId;
+
 
 
 /**
@@ -15,18 +20,21 @@ import seedu.address.model.ids.AvailableServiceId;
  */
 public class DeleteServiceCommand extends Command { public static final String COMMAND_WORD = "deleteservice";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": delete an available service from hotel"
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": delete an available service from hotel by service id"
+            + "or by  the index number used in the displayed room list"
             + "Parameters: "
-            + PREFIX_SERVICEID + "ID "
-            + PREFIX_DESCRIPTION + "DESCRPTION\n"
+            + PREFIX_SERVICEID + "ID\n"
+            + "or \n"
+            + "Parameters: INDEX (must be positive)"
             + "Example: " + COMMAND_WORD + " "
-            + PREFIX_SERVICEID + "WC\n";
+            + PREFIX_SERVICEID + "WC\n"
+            + "Example: " + COMMAND_WORD + " 1";
 
     public static final String MESSAGE_SUCCESS = "The available service with ID: %1$s has been deleted";
     public static final String MESSAGE_SERVICE_NOT_FOUND = "The system has no service with id: %1$s. Please try again";
+    public static final String MESSAGE_INDEX_INVALID = "Invalid index";
 
-    private AvailableServiceId id;
-
+    private final Either<AvailableServiceId, Index> toDelete;
     /**
      * creates an DeleteServiceCommand with given id.
      * @param id: an id
@@ -34,7 +42,16 @@ public class DeleteServiceCommand extends Command { public static final String C
     public DeleteServiceCommand(AvailableServiceId id) {
         requireNonNull(id);
 
-        this.id = id;
+        this.toDelete = Either.ofLeft(id);
+    }
+
+    /**
+     * creates and DeleteServiceCommand with a given index.
+     */
+    public DeleteServiceCommand(Index targetIndex) {
+        requireNonNull(targetIndex);
+
+        this.toDelete = Either.ofRight(targetIndex);
     }
 
     @Override
@@ -43,13 +60,25 @@ public class DeleteServiceCommand extends Command { public static final String C
 
         ReadOnlyHotel hotel = model.getHotel();
 
-        if (hotel.getAvailableServiceList().stream().noneMatch(availableService -> availableService.getId()
-                .equals(this.id))) {
-            throw new CommandException(String.format(MESSAGE_SERVICE_NOT_FOUND, id.toString()));
+        if (this.toDelete.isLeft()) {
+            AvailableServiceId id = this.toDelete.getLeft();
+            if (hotel.getAvailableServiceList().stream().noneMatch(availableService -> availableService.getId()
+                    .equals(id))) {
+                throw new CommandException(String.format(MESSAGE_SERVICE_NOT_FOUND, id.toString()));
+            }
+            model.deleteAvailableService(id);
+            return new CommandResult(String.format(MESSAGE_SUCCESS, id.toString()), "service");
+        } else {
+            List<AvailableService> services = model.getFilteredServiceList();
+            Index index = this.toDelete.getRight();
+            if (index.getZeroBased() >= services.size()) {
+                throw new CommandException(MESSAGE_INDEX_INVALID);
+            }
+            AvailableService service = services.get(index.getZeroBased());
+            model.deleteAvailableService(service.getId());
+            return new CommandResult(String.format(MESSAGE_SUCCESS, service.getId().toString()), "service");
         }
 
-        model.deleteAvailableService(id);
-        return new CommandResult(String.format(MESSAGE_SUCCESS, id.toString()), "service");
     }
 
     @Override
@@ -63,7 +92,7 @@ public class DeleteServiceCommand extends Command { public static final String C
         }
 
         DeleteServiceCommand otherCommand = (DeleteServiceCommand) other;
-        return this.id.equals(otherCommand.id);
+        return this.toDelete.equals(otherCommand.toDelete);
     }
 
 }
