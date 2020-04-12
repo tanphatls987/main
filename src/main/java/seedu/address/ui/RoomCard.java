1ac171bd69a6cbbccb36c7db4bd790f35309fe5b
Daniel Alfred Widjaja <dalfred2000@gmail.com>
2020-04-13 01:02:32 +0800

    fix travis

--- src/main/java/seedu/address/ui/RoomCard.java
+++ src/main/java/seedu/address/ui/RoomCard.java
@@ -1,12 +1,12 @@
 package seedu.address.ui;
 
+import java.time.format.DateTimeFormatter;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
 import seedu.address.model.hotel.room.Room;
-
-import java.time.format.DateTimeFormatter;
 
 /**
  * An UI component that displays information of a {@code Room}.
